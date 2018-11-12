acb27cdeb5823566cfeb843661b54a1fb05b1fe4
Felix Brandt <felixbrandt2512@gmail.com>
2018-11-12 12:56:45 +0100

    worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -3,44 +3,42 @@ workbox.setConfig({modulePathPrefix: "/pwagithubpages/workbox-v3.6.3"});
  
 workbox.core.setCacheNameDetails({prefix: "fapp"});
   
-  workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg|css|js|html)$/,
-    
-    workbox.strategies.cacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    }),
-  );
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg|css|js|html)$/,
   
-  workbox.routing.registerRoute(
-    new RegExp('http://faranto.esn-germany.de/'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'api',
-    }),
-  );
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
 
-  workbox.routing.registerRoute(
-    new RegExp('pwagithubpages/'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'faranto',
-    }),
-  );
-  
-  workbox.routing.registerRoute(
-    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'googleapis',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30,
-        }),
-      ],
-    }),
-  );
-  
-  
+workbox.routing.registerRoute(
+  new RegExp('http://faranto.esn-germany.de/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'api',
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('pwagithubpages/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'faranto',
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
--- src/sw.js
+++ src/sw.js
@@ -1,37 +1,42 @@
  
 workbox.core.setCacheNameDetails({prefix: "fapp"});
   
-  workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
-    
-    workbox.strategies.cacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    }),
-  );
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg|css|js|html)$/,
   
-  workbox.routing.registerRoute(
-    new RegExp('http://faranto.esn-germany.de/'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'api',
-    }),
-  );
-  
-  workbox.routing.registerRoute(
-    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'googleapis',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30,
-        }),
-      ],
-    }),
-  );
-  
-  
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('http://faranto.esn-germany.de/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'api',
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('pwagithubpages/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'faranto',
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
